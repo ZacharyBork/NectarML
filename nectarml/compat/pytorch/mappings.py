f5dbb7be1c9e6517ececa0489394365a83b2ae5c
ZacharyBork <zbork123@gmail.com>
2026-02-26 19:58:16 -0700

    Changed nectarml.compatibility to nectarml.compat

--- nectarml/compat/pytorch/mappings.py
+++ nectarml/compat/pytorch/mappings.py
@@ -1,5 +1,5 @@
-import nectarml
 import torch
+import nectarml
 
 DTYPES_TORCH2NECTAR = {
     torch.float: nectarml.float,
@@ -39,31 +39,3 @@ DTYPES_NECTAR2TORCH = {
     nectarml.uint64: torch.uint64
 }
 
-def convert_dtype(
-    original: torch.dtype | nectarml.DTypeLike
-) -> torch.dtype | nectarml.DTypeLike:
-    DTYPES = DTYPES_TORCH2NECTAR if isinstance(original, torch.dtype) \
-        else DTYPES_NECTAR2TORCH
-    try: new_dtype = DTYPES[original]
-    except: raise ValueError(f'Unrecognized dtype: {original}')
-    return new_dtype
-
-def tensor_torch2nectar(input: torch.Tensor) -> nectarml.Tensor:
-    data = input.numpy()
-    dtype = convert_dtype(input.dtype)
-    device = 'cuda' if input.device.type == 'cuda' else 'cpu'
-    return nectarml.Tensor(
-        data, dtype=dtype, device=device, requires_grad=input.requires_grad)
-    
-def tensor_nectar2torch(input: nectarml.Tensor) -> torch.Tensor:
-    new = torch.Tensor(input.data)
-    new = new.to(input.device, dtype=convert_dtype(input.dtype))
-    new.requires_grad_(input.requires_grad)
-    return new
-
-def module_torch2nectar(input: torch.Tensor) -> nectarml.Tensor:
-    pass
-    
-def module_nectar2torch(input: nectarml.Tensor) -> torch.Tensor:
-    pass
-    
