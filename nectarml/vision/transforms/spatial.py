970e8d5068f066d4ee5b5fd58f9ceda951413c79
ZacharyBork <zbork123@gmail.com>
2026-02-26 17:54:35 -0700

    Added RandomCrop augmentation class

--- nectarml/vision/transforms/spatial.py
+++ nectarml/vision/transforms/spatial.py
@@ -1,3 +1,4 @@
+import random
 from typing import Literal
 from collections.abc import Sequence
 
@@ -82,17 +83,10 @@ class Pad(Transform):
     def run(self, input: Image.Image) -> Image.Image:
         return self.op(input)
 
-class RandomPad(Transform):
-    def __init__(self) -> None:
-        pass
-    
-    def run(self, input: Image.Image) -> Image.Image:
-        pass
-
 class RandomCrop(Transform):
     def __init__(
         self,
-        size: int | Sequence[int],
+        size: int | tuple[int, int],
         padding: int | tuple[int,int] | tuple[int,int,int,int] | None = None,
         pad_if_needed: bool = False,
         fill: float | tuple[float, float, float] = 0.0,
@@ -100,31 +94,42 @@ class RandomCrop(Transform):
             'constant', 'edge', 'reflect', 'symmetric'
         ] = 'constant'
     ) -> None:
-        self.size = size
+        if isinstance(size, int): self.size = (size, size)
+        else: self.size = size
+        
         self.pad_if_needed = pad_if_needed
+        self.fill = fill
         self.padding_mode = padding_mode
-        
-        if isinstance(padding, int):
-            self.do_padding = True
-            self.padding = (padding,) * 4
-        elif isinstance(padding, tuple):
-            self.do_padding = True
-            if len(padding) == 2:
-                self.padding = (padding[0], padding[1], padding[0], padding[1])
-            elif len(padding) == 4: self.padding = padding
-            else: 
-                raise ValueError(
-                    'RandomCrop padding tuple must have either 2 [LT, RB] or '
-                    '4 [L, T, R, B] values.')
-        else: self.do_padding = False
-        
-        if isinstance(fill, float): self.fill = (fill,) * 3
-        else: self.fill = fill
+        if padding is not None: 
+            self.pad = Pad(padding, fill, padding_mode)
+        else: self.pad = None
+    
+    def _validate_input_size(self, input: Image.Image) -> Image.Image:
+        if input.height < self.size[0] or input.width < self.size[1]:
+            if self.pad_if_needed:
+                diff_h = int(np.maximum(0, self.size[0] - input.height))
+                diff_w = int(np.maximum(0, self.size[1] - input.width))
+                padding = (diff_w, diff_h, diff_w, diff_h)
+                self.pad = Pad(padding, self.fill, self.padding_mode)
+                return self.pad(input)
+            else:
+                raise RuntimeError(
+                    f'Input image size {input.size} is greater than '
+                    f'desired crop size: {self.size}')
     
     def run(self, input: Image.Image) -> Image.Image:
-        if self.do_padding:
-            out = ImageOps.expand(input, self.padding, self.fill)
-    
+        if self.pad is None: out = self._validate_input_size(input)
+        else: out = self.pad(input)
+
+        max_offset = (input.height - self.size[0], input.width - self.size[1])
+        offset_h = int(random.random() * max_offset[0])
+        offset_w = int(random.random() * max_offset[1])
+        
+        return out.crop((
+            offset_w, offset_h, 
+            offset_w + self.size[1], 
+            offset_h + self.size[0]))
+        
 class CenterCrop(Transform):
     def __init__(self) -> None:
         pass
