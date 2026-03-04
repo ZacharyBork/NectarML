dd7141264fa9b87653adf9dafa45088d7656640a
ZacharyBork <zbork123@gmail.com>
2026-03-03 21:33:46 -0700

    Split bindings by category

---
+++ csrc/bindings/vision.h
@@ -0,0 +1,22 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include "common.h"
+
+namespace py = pybind11;
+
+/* TRANSFORMS */
+
+py::array_t<uint8_t> hue_shift(py::array_t<uint8_t> image, float shift);
+
+void register_vision(py::module_& m) {
+
+    /* TRANSFORMS */
+
+    m.def("hue_shift", &hue_shift,
+        py::arg("image"),
+        py::arg("shift"),
+        "Shift the hue of an RGB image by the given number of degrees."
+    );
+    
+}
+
