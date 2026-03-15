1da6708e7a1dca846064975616dfa00ebba556bd
ZacharyBork <zbork123@gmail.com>
2026-03-15 15:08:31 -0600

    Added in-place unscale_() method to GradScaler

--- nectarml/amp/grad.py
+++ nectarml/amp/grad.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from nectarml.tensor import Tensor
+from nectarml.typing import float32
 from nectarml.optim.optimizer import Optimizer
 from nectarml.utils import inspection
 
@@ -17,16 +18,25 @@ class GradScaler():
         self.growth_factor = growth_factor
         self.growth_interval = growth_interval
         self._growth_tracker = 0
+        self._unscaled = False
         self._skipped = False
     
     def scale(self: GradScaler, loss: Tensor) -> Tensor:
         self._loss = loss
         return loss * self.scale_factor
     
-    def step(self: GradScaler, optimizer: Optimizer) -> None:
-        found_bad = False
+    def unscale_(self: GradScaler, optimizer: Optimizer) -> None:
+        if self._unscaled: return
         for param in optimizer._get_all_params():
             if param.grad is None: continue
+            param.grad = param.grad.to(dtype=float32) / self.scale_factor
+        self._unscaled = True
+    
+    def step(self: GradScaler, optimizer: Optimizer) -> None:
+        if not self._unscaled: self.unscale_(optimizer)
+        
+        found_bad = False
+        for param in optimizer._get_all_params():
             param.grad = param.grad / self.scale_factor
             if inspection.has_inf(param.grad) or \
                inspection.has_nan(param.grad):
@@ -39,6 +49,7 @@ class GradScaler():
         else: self._skipped = True
         
     def update(self: GradScaler) -> None:
+        self._unscaled = False
         if self._skipped:
             self.scale_factor *= self.backoff_factor            
             self._growth_tracker = 0
