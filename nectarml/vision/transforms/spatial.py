bee321280bd8089337ca3e4bb5dbc8bcfa8df3dd
ZacharyBork <zbork123@gmail.com>
2026-03-22 13:26:09 -0600

    Rebuilt Pad and RandomCrop vision.transforms

--- nectarml/vision/transforms/spatial.py
+++ nectarml/vision/transforms/spatial.py
@@ -5,40 +5,26 @@ from collections.abc import Sequence
 from PIL import Image, ImageOps
 import numpy as np
 
+import nectarml.functional as F
+from nectarml.tensor import Tensor
 from nectarml.vision.transforms import Transform
 
 class Pad(Transform):
     def __init__(
         self,
         padding: int | tuple[int, ...],
-        fill: int | tuple[int, int, int] = 0,
+        fill: float = 0.0,
         padding_mode: Literal[
-            'constant', 'edge', 'reflect', 'symmetric'
-        ] = 'constant'
+            'constant', 'reflect', 'replicate', 'circular'
+        ] = 'constant',
     ) -> None:
         super().__init__()
         self.padding: tuple[int, ...] = None
-        self.is_grayscale = False
+        self.fill = fill
+        self.padding_mode = padding_mode
+        
         self._init_padding(padding)
-        self._init_fill(fill)
-        
-        match padding_mode:
-            case 'constant': self.op = self._constant
-            case 'edge': self.op = self._edge
-            case 'reflect': self.op = self._reflect
-            case 'symmetric': self.op = self._symmetric
-       
-    ### PROPERTIES ###
-    
-    @property
-    def np_pad_width(self) -> tuple[tuple[int, int], ...]:
-        p = self.padding
-        width = ((p[1], p[3]), (p[0], p[2]))
-        if not self.is_grayscale: width += ((0, 0),)
-        return width
-    
-    ### UTILS ###
-        
+                
     def _init_padding(self, padding: int | tuple[int, ...]) -> None:
         if isinstance(padding, int): self.padding = (padding,) * 4
         elif isinstance(padding, tuple):            
@@ -50,47 +36,17 @@ class Pad(Transform):
                     'RandomCrop padding tuple must have either 2 [LT, RB] or '
                     '4 [L, T, R, B] values.')
         else: raise ValueError('Pad.padding must be int or tuple[int, ...].')
-                    
-    def _init_fill(self, fill: int | tuple[int, int, int]) -> None:
-        if isinstance(fill, int): self.fill = (fill,) * 3
-        else: self.fill = fill
-    
-    def _pad_numpy(
-        self, 
-        input: Image.Image, 
-        mode: Literal['edge', 'reflect', 'symmetric']
-    ) -> Image.Image:
-        array = np.array(input)
-        self.is_grayscale = array.ndim == 2
-        padded = np.pad(array, self.np_pad_width, mode=mode)
-        return Image.fromarray(padded)
-    
-    ### PADDING MODES ###
-    
-    def _constant(self, input: Image.Image) -> Image.Image:
-        return ImageOps.expand(input, self.padding, self.fill)
-    
-    def _edge(self, input: Image.Image) -> Image.Image:
-        return self._pad_numpy(input, mode='edge')
         
-    def _reflect(self, input: Image.Image) -> Image.Image:
-        return self._pad_numpy(input, mode='reflect')
+    def forward(self, input: Tensor) -> Tensor:
+        return F.pad(input, self.padding, self.padding_mode, self.fill)
     
-    def _symmetric(self, input: Image.Image) -> Image.Image:
-        return self._pad_numpy(input, mode='symmetric')
-    
-    ### forward ###
-    
-    def forward(self, input: Image.Image) -> Image.Image:
-        return self.op(input)
-
 class RandomCrop(Transform):
     def __init__(
         self,
         size: int | tuple[int, int],
-        padding: int | tuple[int,int] | tuple[int,int,int,int] | None = None,
+        padding: int | tuple[int, ...] | None = None,
         pad_if_needed: bool = False,
-        fill: float | tuple[float, float, float] = 0.0,
+        fill: float = 0.0,
         padding_mode: Literal[
             'constant', 'edge', 'reflect', 'symmetric'
         ] = 'constant'
@@ -102,132 +58,132 @@ class RandomCrop(Transform):
         self.pad_if_needed = pad_if_needed
         self.fill = fill
         self.padding_mode = padding_mode
-        if padding is not None: 
-            self.pad = Pad(padding, fill, padding_mode)
+        if padding is not None: self.pad = Pad(padding, fill, padding_mode)
         else: self.pad = None
     
-    def _validate_input_size(self, input: Image.Image) -> Image.Image:
-        if input.height < self.size[0] or input.width < self.size[1]:
+    def _validate_input_size(self, input: Tensor) -> Tensor:
+        B, C, H, W = input.shape
+        if H < self.size[0] or W < self.size[1]:
             if self.pad_if_needed:
-                diff_h = int(np.maximum(0, self.size[0] - input.height))
-                diff_w = int(np.maximum(0, self.size[1] - input.width))
+                diff_h = int(np.maximum(0, self.size[0] - H))
+                diff_w = int(np.maximum(0, self.size[1] - W))
                 padding = (diff_w, diff_h, diff_w, diff_h)
                 self.pad = Pad(padding, self.fill, self.padding_mode)
                 return self.pad(input)
             else:
                 raise RuntimeError(
-                    f'Input image size {input.size} is greater than '
+                    f'Input image size {input.shape[2:]} is greater than '
                     f'desired crop size: {self.size}')
         else: return input
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         if self.pad is None: out = self._validate_input_size(input)
         else: out = self.pad(input)
 
-        max_offset = (input.height - self.size[0], input.width - self.size[1])
+        max_offset = (input.shape[2]-self.size[0], input.shape[3]-self.size[1])
         offset_h = int(random.random() * max_offset[0])
         offset_w = int(random.random() * max_offset[1])
         
-        return out.crop((
-            offset_w, offset_h, 
-            offset_w + self.size[1], 
-            offset_h + self.size[0]))
+        return out[
+            :, :, 
+            offset_h:offset_h+self.size[0], 
+            offset_w:offset_w+self.size[1]]
         
 class CenterCrop(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
     
 class RandomResizedCrop(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
     
 class Resize(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class RandomHorizontalFlip(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class RandomVerticalFlip(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class RandomRotation(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class RandomAffine(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class RandomPerspective(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class ElasticTransform(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class GridDistortion(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class OpticalDistortion(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class FiveCrop(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class TenCrop(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
 class RandomCropNearBBox(Transform):
     def __init__(self) -> None:
         super().__init__()
     
-    def forward(self, input: Image.Image) -> Image.Image:
+    def forward(self, input: Tensor) -> Tensor:
         pass
 
