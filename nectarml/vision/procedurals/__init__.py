0b28718fc2759eb0e7a0dc485c07c2382c844623
ZacharyBork <zbork123@gmail.com>
2026-04-06 18:09:23 -0600

    Added ChladniCymaticPatterns to vision.procedurals

--- nectarml/vision/procedurals/__init__.py
+++ nectarml/vision/procedurals/__init__.py
@@ -1,5 +1,5 @@
 from .generator import Generator
 from .noise import Noise
-from .shapes import Checkerboard
+from .shapes import Checkerboard, ChladniCymaticPatterns
 from .colors import Solid, Gradient
 from .sdf import SdfCreate, SdfCombine, SdfToGray, SdfToColor, SdfColorRamp
