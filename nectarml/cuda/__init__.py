c5468a8a0b2cd2ab7dcd35714b5a44089e5ffdd2
ZacharyBork <zbork123@gmail.com>
2026-04-23 21:02:14 -0600

    Restructured nectarml.cuda submodule

--- nectarml/cuda/__init__.py
+++ nectarml/cuda/__init__.py
@@ -1,28 +1,26 @@
-from . import (
-    amp,
-    combination, 
-    conv, 
-    indexing, 
-    interpolation,
-    masking,
-    math, 
-    matmul,
-    padding,
-    pooling,
-    reductions,
-    shapes,
-    sorting,
-    utils
+from . import amp, utils
+
+from .tensor import (
+    combination, conv, indexing, interpolation, masking, math, matmul,
+    padding, pooling, reductions, shapes, sorting
 )
 
 from .utils import (
-    is_cuda_available, get_cuda_info, cuda_synchronize, 
-    cast_tensor, cast_tensor_by_reference, to_cuda, data_to_cuda, 
-    to_cpu, inspect_cuda_data, clone, is_inf, is_finite, is_nan, 
-    has_inf, has_nan)
+    is_cuda_available, get_cuda_info, 
+    
+    cuda_synchronize, inspect_cuda_data,
+    
+    is_inf, is_finite, is_nan, has_inf, has_nan,
+    
+    cast_tensor, cast_tensor_by_reference, 
+    to_cuda, data_to_cuda, to_cpu, clone,
+)
 
 from .memory import (
     enable_allocator_pool, disable_allocator_pool, release_allocator_pool,    
+    
     free_cuda, alloc_cuda_full, alloc_cuda_random, 
-    get_cuda_meminfo, memory_allocated, get_memory_statistics)
+   
+    get_cuda_meminfo, memory_allocated, get_memory_statistics
+)
 
---
+++ nectarml/cuda/tensor/__init__.py
@@ -0,0 +1,14 @@
+from . import (
+    combination, 
+    conv, 
+    indexing, 
+    interpolation,
+    masking,
+    math, 
+    matmul,
+    padding,
+    pooling,
+    reductions,
+    shapes,
+    sorting
+)
