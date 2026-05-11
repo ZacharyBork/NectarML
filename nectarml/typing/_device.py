7d3cdbdbf8564d4f2a5e0ebe36edd35f0589c618
ZacharyBork <zbork123@gmail.com>
2026-05-11 13:57:40 -0600

    Added some docstrings for typing classes

--- nectarml/typing/_device.py
+++ nectarml/typing/_device.py
@@ -5,7 +5,7 @@ if TYPE_CHECKING:
     
 import builtins
 from   dataclasses import dataclass
-from   typing      import Self, Literal, overload
+from   typing      import Self, Literal
 
 ### DEVICE CLASS ###
 
@@ -18,9 +18,51 @@ class device:
 
     def __new__(
         cls:       type[Self], 
-        type:      DeviceLikeType | builtins.str, 
-        device_id: builtins.int   | None = None
-    ) -> None:
+        type:      DeviceLikeType, 
+        device_id: builtins.int | None = None
+    ) -> Self:
+        '''Sets device id for CUDA and checks device cache to avoid duplicates.
+
+        If passed another `device` object for initialization, this method will
+        inherit the device id of the given `device`. If passed a string (i.e.
+        "cuda", "cpu"), and if `device_id` if not provided, this method will 
+        set the device id to None for CPU tensors, and 0 for CUDA tensors. 
+        Otherwise it will set the device id of the new `device` to the 
+        specified `device_id`.
+
+        This method will also check against the `_DEVICE_CACHE` upon creation
+        of a new `device` object to see if an existing `device` object with the
+        same specifications already exists. If so, it will instead return a 
+        reference to the existing object in the cache to avoid the unnecessary 
+        creation of a duplicate `device`.
+
+        Please note that the above means that any time a new `device` is 
+        initialized by passing an existing device, and a `device_id` is not
+        specified, this method will always return a reference to the device 
+        passed for initialization. This allows you to do things like:
+        
+        ```
+        from nectarml.typing import device, DeviceLikeType
+
+        def make_device(
+            in_device: DeviceLikeType, 
+            device_id: int | None = None
+        ) -> device:
+            return device(in_device, device_id)
+
+        x = make_device('cuda') # Makes new cuda device and assigns to x
+        y = make_device(x)      # Same function now makes y a reference to "x"
+        ```
+        
+        Args:
+            type      : The device type for the new `device` object. Can be 
+                        another `device` object, or ["cpu", "cuda"]
+            device_id : The device id for the new device, or None. Only used 
+                        for CUDA devices.
+        
+        Returns:
+            device : The new (or cached) device object.
+        '''
         if isinstance(type, device):
             type      = type.type
             device_id = device_id if device_id is not None else type.device_id
@@ -35,19 +77,19 @@ class device:
         _DEVICE_CACHE[key] = obj
         return obj
 
-    @overload
-    def __init__(self, type: DeviceLikeType) -> None: ...
-    @overload
     def __init__(
         self:      device, 
-        type:      builtins.str, 
-        device_id: builtins.int | None = None
-    ) -> None: ...
-    def __init__(
-        self:      device, 
-        type:      builtins.str,
+        type:      DeviceLikeType,
         device_id: builtins.int | None = None
     ) -> None:
+        '''Initailizes a new device object.
+
+        Args:
+            type      : The device type for the new `device` object. Can be 
+                        another `device` object, or ["cpu", "cuda"]
+            device_id : The device id for the new device, or None. Only used 
+                        for CUDA devices.
+        '''
         if hasattr(self, 'type'): return
         
         if isinstance(type, device):
@@ -63,6 +105,32 @@ class device:
         super().__setattr__('device_id', device_id)
     
     def __eq__(self: device, other: DeviceLikeType) -> builtins.bool:
+        '''Checks if a DeviceLikeType object is the same as a given device.
+
+        Can check device<->device and device<->string. So both of these are
+        valid:
+        ```
+        from nectarml.typing import device
+        
+        x = device('cuda')
+        y = device('cpu')
+        print(x == y) # Prints "False"
+        ```
+        And:
+        ```
+        from nectarml.typing import device
+        
+        x = device('cuda')
+        print(x == 'cuda') # Prints "True"
+        ```
+        
+        Args:
+            other : The DeviceLikeType object to check the device against.
+        
+        Returns:
+            bool : True if the given device is equal to the DeviceLikeType 
+                   object being compared agains, otherwise False.
+        '''
         if isinstance(other, str):
             return self.type == other
         if isinstance(other, device):
--- nectarml/typing/_dtype.py
+++ nectarml/typing/_dtype.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from nectarml.typing import DTypeLike
-    
+
 import builtins
 from   dataclasses import dataclass
 from   typing      import Self
@@ -20,7 +17,7 @@ class iinfo:
     max:   builtins.int
     dtype: dtype
     
-    def __repr__(self: finfo) -> str:
+    def __repr__(self: finfo) -> builtins.str:
         return (
             f'Bits:  {self.bits}\n'
             f'Min:   {self.min}\n'
@@ -38,7 +35,7 @@ class finfo:
     resolution:      builtins.float
     dtype:           dtype
     
-    def __repr__(self: finfo) -> str:
+    def __repr__(self: finfo) -> builtins.str:
         return (
             f'Bits:            {self.bits}\n'
             f'Min:             {self.min}\n'
@@ -54,6 +51,7 @@ class finfo:
 
 @dataclass(frozen=True)
 class DTypeMap:
+    '''Helper class to map numpy DTypes to NectarML host DTypes.'''
     mappings = {
         np.float32: _nectarml.DType.Float32,
         np.float16: _nectarml.DType.Float16,
@@ -72,10 +70,29 @@ class DTypeMap:
 _DTYPE_MAP  = DTypeMap()
 _DTYPE_CACHE: dict[np.dtype, dtype] = {}
 
-class dtype:  
+class dtype:
     _map = _DTYPE_MAP
     
-    def __new__(cls: type[Self], type_cpu: DTypeLike = None) -> Self:
+    def __new__(cls: type[Self], type_cpu: np.typing.DTypeLike = None) -> Self:
+        '''Checks in new DType exists in cache and uses cached instead if so.
+
+        Avoids unnecessary creation of duplicate DTypes while also allowing for
+        new mechanisms of DType assignment. This method just checks upon 
+        creation of a new `dtype` object whether the `_DTYPE_CACHE` already
+        contains a `dtype` object with the corresponding `type_cpu`. If so, it 
+        retrieves the cached version and returns a reference to that instead, 
+        rather than creating a new `dtype` object. If not, it creates a new 
+        `dtype` object for the given `type_cpu`, adds it to the `_DTYPE_CACHE`,
+        the returns a reference to the newly created/cached `dtype`.
+
+        Args:
+            type_cpu : The np.typing.DTypeLike object to serve as the new 
+                       `dtype` object's CPU DType. CUDA DType is mapped 
+                       automatically from this.
+                    
+        Returns:
+            dtype : The new (or cached) dtype object.
+        '''
         if type_cpu is None:         return object.__new__(cls)
         if type_cpu in _DTYPE_CACHE: return _DTYPE_CACHE[type_cpu]
         
@@ -83,7 +100,14 @@ class dtype:
         _DTYPE_CACHE[type_cpu] = obj
         return obj
     
-    def __init__(self: dtype, type_cpu: DTypeLike) -> None:
+    def __init__(self: dtype, type_cpu: np.typing.DTypeLike) -> None:
+        '''Initializes a new dtype object.
+
+        Args:
+            type_cpu : The np.typing.DTypeLike object to serve as the new 
+                       `dtype` object's CPU DType. CUDA DType is mapped 
+                       automatically from this.
+        '''
         self._type_cpu         = type_cpu
         self.is_floating_point = np.issubdtype(type_cpu, np.floating)
         self.is_complex        = np.issubdtype(type_cpu, np.complexfloating)
@@ -95,6 +119,7 @@ class dtype:
     # INIT
     
     def _build_info(self: dtype) -> None:
+        '''Builds `iinfo` or `finfo` object for new dtype.'''
         if self._type_cpu in [np.bool_]: return None
         
         if self.is_floating_point:
@@ -109,40 +134,111 @@ class dtype:
     # PROPERTIES
     
     @property
-    def numpy(self: dtype) -> np.typing.DTypeLike: 
+    def numpy(self: dtype) -> np.typing.DTypeLike:
+        '''Returns the corresponding CPU/numpy DType for the `dtype` object.
+
+        Returns:
+            np.typeing.DTypeLike : The numpy DType for the given `dtype`.
+        '''
         return self._type_cpu
     
     @property
     def cpu (self: dtype) -> np.typing.DTypeLike: 
+        '''Returns the corresponding CPU/numpy DType for the `dtype` object.
+
+        Returns:
+            np.typeing.DTypeLike : The numpy DType for the given `dtype`.
+        '''
         return self._type_cpu
     
     @property
     def cuda(self: dtype) -> _nectarml.DType:
+        '''Returns the CUDA DType for the given `dtype` object.
+
+        Returns:
+            _nectarml.DType : The nectarml.DType which corresponds to the 
+                              datatype for the given `dtype` object. 
+                              _nectarml.DType is an enum is the host layer. 
+                              This is the DType reference expected by C++ ops 
+                              and CUDA kernels. See here for more info:
+                                - `nectarml/csrc/include/common/dtype.h`
+        '''
         return self._map.map_dtype(self.cpu)
     
     @property
-    def name(self: dtype) -> str:
+    def name(self: dtype) -> builtins.str:
+        '''Returns the name of the given DType as a string.
+
+        Example:
+            `nectarml.float32.name`: `"float32"`
+        
+        Returns:
+            str : The name of the `dtype` as a string.
+        '''
         return self._type_cpu.__name__
 
     # COMPARISON
     
-    def __hash__(self): return hash(self._type_cpu)
+    def __hash__(self: dtype) -> builtins.int: 
+        '''Dtype hashing method.
 
-    def __eq__(self, other):
+        This method just returns the hash of the underlying numpy DType object 
+        for the given `dtype`. Since each `dtype` can only ever own one unique 
+        numpy DType (due to the __new__ behavior of the `dtype` class), each 
+        `dtype` just inherits the hash id of said DType to keep things simple.
+
+        Returns:
+            int : The hash of the numpy DType object for the given `dtype`.
+        '''
+        return hash(self._type_cpu)
+
+    def __eq__(self: dtype, other: dtype) -> builtins.bool:
+        '''Checks whether two DTypes are the same.
+
+        Checks whether this `dtype` and the other `dtype` have the same 
+        underlying numpy DType. Every `dtype` should only ever own one numpy
+        DType which is unique to itself (due to the __new__ behavior of the 
+        `dtype` class), so doing it this way is not strictly necessary. But 
+        this avoids issues should a new `dtype` ever get created with an 
+        underlying numpy DType which is the same as another `dtype` object for
+        whatever reason.
+
+        Args:
+            dtype : The other `dtype` to compare the given `dtype` against.
+            
+        Returns:
+            bool : True if the two `dtype`s are the same, otherwise False.
+        '''
         if isinstance(other, dtype):
             return self._type_cpu == other._type_cpu
         return NotImplemented
     
     def __ne__(self: dtype, other: dtype) -> builtins.bool:
+        '''Checks whether two DTypes are not the same.
+
+        Checks whether this `dtype` and the other `dtype` do not have the same 
+        underlying numpy DType. Every `dtype` should only ever own one numpy
+        DType which is unique to itself (due to the __new__ behavior of the 
+        `dtype` class), so doing it this way is not strictly necessary. But 
+        this avoids issues should a new `dtype` ever get created with an 
+        underlying numpy DType which is the same as another `dtype` object for
+        whatever reason.
+
+        Args:
+            dtype : The other `dtype` to compare the given `dtype` against.
+            
+        Returns:
+            bool : True if the two `dtype`s are not the same, otherwise False.
+        '''
         if isinstance(other, dtype):
             return self._type_cpu != other._type_cpu
         return NotImplemented
 
     # INSPECTION
     
-    def __str__ (self: dtype) -> str: return f'nectarml.{self.name}'
-    def __repr__(self: dtype) -> str: return self.__str__()
-    def __reduce__(self): return (dtype, (self._type_cpu,))
+    def __str__   (self: dtype) -> str:  return f'nectarml.{self.name}'
+    def __repr__  (self: dtype) -> str:  return self.__str__()
+    def __reduce__(self: dtype) -> None: return (dtype, (self._type_cpu,))
 
 ### DTYPE INSTANCES ###
 
